9cc16a48b62c2b7b872c43ef6e12ae7966f1e2ac
harry <harrymac1972@gmail.com>
2024-02-10 15:39:23 -0700

    employees table schema added

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,13 @@ CREATE TABLE roles (
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
