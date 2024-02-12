67e75bb1d5876dbb10902e5c00522aca9a14f664
harry <harrymac1972@gmail.com>
2024-02-11 22:34:28 -0700

    seeds.sql roles seeded

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,14 @@ VALUES ('Engineering'),
        ('Legal'),
        ('Sales'),
        ('Service');
+
+INSERT INTO roles (title,salary,department_id) 
+VALUES ('Sales Lead',100000,4),
+       ('Salesperson',80000,4),
+       ('Lead Engineer',150000,1),
+       ('Software Engineer',12000,1),
+       ('Account Manager',160000,2),
+       ('Accountant',125000,2),
+       ('Legal Team Lead',250000,3),
+       ('Lawyer',190000,3);
+
